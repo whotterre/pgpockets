bcdb9afce53d08ffb35d963b633ed72000aa4ccb
whotterre <iJeddy5.0@gmail.com>
2025-06-25 12:48:32 +0100

    add config loading functionality

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+	"pgpockets/internal/config"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
 func main(){
-	
+	app := fiber.New()
+	// Twelve factor apps need logs
+	app.Use(logger.New())
+	config, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("cannot load config: %v", err)
+	}
+	log.Fatal(app.Listen(config.ServerAddr))
 }
---
+++ internal/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBSource            string `mapstructure:"DB_SOURCE"`
+	ServerAddr          string `mapstructure:"SERVER_ADDR"`
+}
+
+func LoadConfig() (config Config, err error) {
+	viper.AddConfigPath("../")
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
